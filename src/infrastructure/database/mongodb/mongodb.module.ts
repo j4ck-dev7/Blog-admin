e030ce71edc618a3cf1894f4a99c641808196293
Jackson <j4ckson7dev@gmail.com>
2026-07-24 08:28:09 -0300

    Configuração principal dos banco de dados concluido

--- src/infrastructure/database/mongodb/mongodb.module.ts
+++ src/infrastructure/database/mongodb/mongodb.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { databaseProviders } from '../../../config/database.config';
+import { databaseProviders } from '../database.config';
 
 @Module({
   providers: [
--- src/infrastructure/database/postgres/postgres.module.ts
+++ src/infrastructure/database/postgres/postgres.module.ts
@@ -1,9 +1,10 @@
-import { Module } from "@nestjs/common";
-import { databaseProviders } from "../../../config/database.config";
+import { Module } from '@nestjs/common';
+import { databaseProviders } from '../database.config';
 
 @Module({
-  providers: [...databaseProviders.filter(p => p.provide.includes('POSTGRES_POOL'))],
+  providers: [
+    ...databaseProviders.filter((p) => p.provide.includes('POSTGRES_POOL')),
+  ],
   exports: ['POSTGRES_POOL'],
 })
-
 export class PostgresModule {}
